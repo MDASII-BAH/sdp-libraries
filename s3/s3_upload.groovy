13a3d28c5cc4688455c94ab96a0115877b23eea1
Carlos OKieffe <okieffe_carlos@bah.com>
2019-08-14 13:26:23 -0400

    reverting upload to region

--- s3/s3_upload.groovy
+++ s3/s3_upload.groovy
@@ -4,7 +4,7 @@ void call(Map params = [:] ){
 
   params.bucket = config.bucket ?: params.bucket
 
-  withAWS(credentials:'AwsBsdo', endpointUrl:'s3.us-east-1.amazonaws.com') {
+  withAWS(credentials:'AwsBsdo', region: 'us-east-1') {
     s3Upload(params)
   }
 
