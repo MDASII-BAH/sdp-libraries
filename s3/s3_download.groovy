bad97aebeb3c8862dd41f2d473eb579688c796e0
Carlos OKieffe <okieffe_carlos@bah.com>
2019-08-14 14:23:31 -0400

    trying download withAws(Map)

--- s3/s3_download.groovy
+++ s3/s3_download.groovy
@@ -1,10 +1,14 @@
 void call(Map params = [:] ){
   def url = config.url
   def bucket = config.bucket
+  def region = config.region ?: params.region
 
   params.bucket = config.bucket ?: params.bucket
 
-  withAWS(credentials:'AwsBsdo', region:'us-east-1') {
+  def awsConfig = [credentials: 'AwsBsdo']
+  awsConfig.region = config.region ?: params.region
+
+  withAWS(awsConfig) {
     s3Download(params)
   }
 
