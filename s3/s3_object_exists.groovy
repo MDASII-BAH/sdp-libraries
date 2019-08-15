6a7c11c19a0c09798a537c27254a68ad60569963
Carlos OKieffe <okieffe_carlos@bah.com>
2019-08-15 15:22:50 -0400

    adding s3_object_exists

--- s3/s3_object_exists.groovy
+++ s3/s3_object_exists.groovy
@@ -1,4 +1,6 @@
 boolean call(Map params = [:]){
+  params.bucket = config.bucket ?: params.bucket
+
   def awsConfig = [:]
   awsConfig.credentials = config.credentials ?: params.credentials
   awsConfig.region = config.region ?: params.region
