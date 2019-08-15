6c379648fa0e479005f8c6c9fe9f09cfe22f80f4
Carlos OKieffe <okieffe_carlos@bah.com>
2019-08-15 14:49:22 -0400

    adding s3_copy

---
+++ s3/s3_copy.groovy
@@ -0,0 +1,12 @@
+void call(Map params = [:]){
+  params.bucket = config.bucket ?: params.bucket
+
+  def awsConfig = [:]
+  awsConfig.credentials = config.credentials ?: params.credentials
+  awsConfig.region = config.region ?: params.region
+
+  withAWS(awsConfig) {
+    s3Copy(params)
+  }
+
+}
