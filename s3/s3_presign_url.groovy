c276c85458064346eebd05ec85be59bee606ab9b
Carlos OKieffe <okieffe_carlos@bah.com>
2019-08-16 08:37:18 -0400

    added s3_presign_url

---
+++ s3/s3_presign_url.groovy
@@ -0,0 +1,12 @@
+def call(Map params = [:]){
+  params.bucket = config.bucket ?: params.bucket
+
+  def awsConfig = [:]
+  awsConfig.credentials = config.credentials ?: params.credentials
+  awsConfig.region = config.region ?: params.region
+
+  withAWS(awsConfig) {
+    return s3PresignURL(params)
+  }
+
+}
